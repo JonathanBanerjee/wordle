e143aa25e6605faeff38180971eb65b79f8272a1
jonathanbanerjee <jonathan@youd.net>
2022-12-18 21:08:34 +0000

    adding a while loop

--- create_db.sh
+++ create_db.sh
@@ -15,18 +15,20 @@ docker volume rm wordle_dbdata
 echo "Now running the postgres container"
 container_id=$(docker run -it -d  -e POSTGRES_USER=${pg_user} -e POSTGRES_DB=${pg_db} -e POSTGRES_PASSWORD=${pg_pass} -v $PWD:/opt/sql -v wordle_dbdata:/var/lib/postgresql/data postgres:13) 
 
-
+while ! docker exec ${container_id} pg_isready; do
+  # Print a message and sleep for 3 seconds before checking again
+  echo "Waiting for the database to start"
+  sleep 1
+done
 
 
 echo "Container has been made with a volume"
 # Run psql to create the database, substituting the name of the container.
 sleep 5
 
-# DOCKER_CONTAINER_NAME="mypgcontainer"
-# timeout 90s bash -c "until docker exec $DOCKER_CONTAINER_NAME pg_isready ; do sleep 5 ; done
-
 docker exec -it ${container_id} psql -U ${pg_user} ${pg_db} -f /opt/sql/draft-db.sql
-docker exec ${container_id} pg_isready
+
+
 
 echo "This volume now contains data"
 
