a3f3e9c87a28ac83ec053f1c60b020f3563f9ba5
jonathanbanerjee <jonathan@youd.net>
2022-12-24 14:40:34 +0000

    adding exit status checks

--- create_db.sh
+++ create_db.sh
@@ -11,15 +11,26 @@ if [[ $(docker volume ls -f name=wordle_dbdata) == "wordle_dbdata" ]]; then dock
 # Remove the volume
 docker volume rm wordle_dbdata
 
-# Runs the POSTGRES container
+# Run the POSTGRES container
 echo "Now running the postgres container"
 container_id=$(docker run -it -d  -e POSTGRES_USER=${pg_user} -e POSTGRES_DB=${pg_db} -e POSTGRES_PASSWORD=${pg_pass} -v $PWD:/opt/sql -v wordle_dbdata:/var/lib/postgresql/data postgres:13) 
 
-while ! docker exec ${container_id} pg_isready; do
+while true; do
+  docker exec ${container_id} pg_isready
+  if [[ $? -eq 0 ]]; then
+    # The server is ready, so exit the loop
+    break
+  fi
   # Print a message and sleep for 3 seconds before checking again
-  echo "Waiting for the database to start"
-  sleep 1
+  echo "Waiting for the database to start..."
+  sleep 3
 done
+
+# Check the exit status of the docker exec command
+if [[ $? -ne 0 ]]; then
+  echo "Error: docker exec command failed"
+  exit 1
+fi
 
 
 echo "Container has been made with a volume"
